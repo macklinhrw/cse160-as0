855341382f3ab7561a4ce3daaae4bb162501e8dd
macklinhrw <macklingaming@gmail.com>
2025-01-11 14:35:22 -0800

    feat: finish part 3

--- asg0.ts
+++ asg0.ts
@@ -1,9 +1,31 @@
 import { Vector3 } from "./lib/cuon-matrix-cse160";
 
-// DrawRectangle.js
+// References:
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/canvas
 function main() {
-  // Retrieve <canvas> element
-  var canvas = document.getElementById("example") as HTMLCanvasElement;
+  // on click listener
+  const drawButton = document.getElementById("draw");
+  drawButton?.addEventListener("click", handleDrawEvent);
+
+  const canvasResult = getCanvasAndContext();
+  if (canvasResult == undefined) {
+    console.log("Failed to retrieve canvas and context.");
+    return;
+  }
+  const { canvas, context: ctx } = canvasResult;
+
+  clearCanvas(canvas, ctx);
+
+  // draw vector
+  let v1 = new Vector3([2.25, 2.25, 0]);
+  drawVector(v1, "red", ctx);
+
+  // ctx.fillStyle = "rgba(0, 0, 255, 1.0)"; // Set a blue color
+  // ctx.fillRect(120, 10, 150, 150); // Fill a rectangle with the color
+}
+
+const getCanvasAndContext = () => {
+  var canvas = document.getElementById("example") as HTMLCanvasElement | null;
   if (!canvas) {
     console.log("Failed to retrieve the <canvas> element");
     return;
@@ -16,18 +38,20 @@ function main() {
     return;
   }
 
-  // draw vector
-  let v1 = new Vector3([2.25, 2.25, 0]);
-  drawVector(v1, "red", ctx);
+  return { canvas, context: ctx };
+};
 
-  // ctx.fillStyle = "rgba(0, 0, 255, 1.0)"; // Set a blue color
-  // ctx.fillRect(120, 10, 150, 150); // Fill a rectangle with the color
+function clearCanvas(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
 // References:
 // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/stroke
 // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/lineTo
 function drawVector(v: Vector3, color: string, ctx: CanvasRenderingContext2D) {
+  // store so we can restore later
+  let oldStrokeStyle = ctx.strokeStyle;
+
   ctx.strokeStyle = color;
   ctx.beginPath(); // Start a new path
   // Center of canvas
@@ -35,11 +59,37 @@ function drawVector(v: Vector3, color: string, ctx: CanvasRenderingContext2D) {
   let centerY = 200;
   ctx.moveTo(centerX, centerY);
   // NOTE: 0,0 is in topleft
-  // Add centerX, subtract centerY
+  // Add centerX, subtract from centerY
   ctx.lineTo(centerX + v.elements[0] * 20, centerY - v.elements[1] * 20);
   ctx.stroke();
+
+  // restore stroke style
+  ctx.strokeStyle = oldStrokeStyle;
+}
+
+function handleDrawEvent() {
+  // get inputs
+  const v1xButton = document.getElementById("v1x") as HTMLInputElement | null;
+  const v1yButton = document.getElementById("v1y") as HTMLInputElement | null;
+  if (!v1xButton || !v1yButton) {
+    console.error("Couldn't find elements v1x and v1y");
+    return;
+  }
+
+  const v1x = Number(v1xButton.value);
+  const v1y = Number(v1yButton.value);
+
+  let v1 = new Vector3([v1x, v1y, 0]);
+
+  const canvasResult = getCanvasAndContext();
+  if (canvasResult == undefined) {
+    console.log("Failed to retrieve canvas and context.");
+    return;
+  }
+  const { canvas, context: ctx } = canvasResult;
+
+  clearCanvas(canvas, ctx);
+  drawVector(v1, "red", ctx);
 }
 
 main();
-
-export {};
