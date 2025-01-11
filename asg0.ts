715d72031de8f61596810f2b91c3791cd662634f
macklinhrw <macklingaming@gmail.com>
2025-01-10 18:13:42 -0800

    feat: setup modules for lib and types, fix importing it etc

--- asg0.ts
+++ asg0.ts
@@ -1,3 +1,5 @@
+import { Vector3 } from "./lib/cuon-matrix-cse160";
+
 // DrawRectangle.js
 function main() {
   // Retrieve <canvas> element                                <- (1)
@@ -15,6 +17,19 @@ function main() {
   }
 
   // Draw a blue rectangle                                   <- (3)
-  ctx.fillStyle = "rgba(0, 0, 255, 1.0)"; // Set a blue color
-  ctx.fillRect(120, 10, 150, 150); // Fill a rectangle with the color
+  let v1 = new Vector3([0, 0, 0]);
+  console.log(v1);
+  drawVector(v1, "red", ctx);
+
+  // ctx.fillStyle = "rgba(0, 0, 255, 1.0)"; // Set a blue color
+  // ctx.fillRect(120, 10, 150, 150); // Fill a rectangle with the color
 }
+
+function drawVector(v: Vector3, color: string, ctx: CanvasRenderingContext2D) {
+  ctx.fillStyle = color;
+  ctx.lineTo(v.elements[0] * 0, v.elements[1] * 20);
+}
+
+main();
+
+export {};
---
+++ lib/cuon-matrix-cse160.d.ts
@@ -0,0 +1,14 @@
+export class Vector3 {
+  constructor(opt_src: any);
+  elements: Float32Array;
+}
+
+export class Vector4 {
+  constructor(opt_src: any);
+  elements: Float32Array;
+}
+
+export class Matrix4 {
+  constructor(opt_src: any);
+  elements: Float32Array;
+}
